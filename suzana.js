70b4d3e6339559c5dafc626ad25bab147b338bd3
Stelian Dumitrascu <elviestelian@gmail.com>
2026-06-07 13:14:28 -0400

    Delete suzana.js

--- suzana.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  fetch('/geocentral/suzana/menu.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('menu').innerHTML = data;
-    });
-}
-
