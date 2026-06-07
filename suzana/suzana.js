bad5544f5e6eeb1ff155808407bb7a4b57f770fe
Stelian Dumitrascu <elviestelian@gmail.com>
2026-06-07 13:08:15 -0400

    Add files via upload

--- suzana/suzana.js
+++ suzana/suzana.js
@@ -1,5 +1,7 @@
+window.basePath = '/geocentral';
+
 window.onload = function() {
-  fetch('/suzana/menu.html')
+  fetch('suzana/menu.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('menu').innerHTML = data;
